63751bfe58722d9e4bc101bc7ee9804b5844a293
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-07-14 19:27:02 -0400

    Merge branch 'baseline' of https://github.com/bahrus/co-pair into baseline

--- dir.js
+++ dir.js
@@ -1,5 +1,37 @@
-export class dir{
-    '<mount>'(el){
-        console.log('dir attach', el);
+//@ts-check
+/** @import {DirProps, DirActions} from './types' */
+/** @import {IshConfig } from './ts-refs/trans-render/froop/types' */
+
+import {Scope} from 'trans-render/froop/Scope.js';
+
+/**
+ * @implements {DirActions}
+ */
+export class Dir extends Scope{
+    /**
+     * @type {IshConfig<DirProps, DirActions>}
+     */
+    static config = {
+        propInfo: {
+            beDirective: {}
+        },
+        compacts:{
+            "when_beDirective_changes_call_hydrate":0
+        }
     }
+
+    /**
+     * 
+     * @param {DirProps} self 
+     */
+    hydrate(self){
+        const {beDirective} = self;
+        console.log({beDirective});
+    }
+
+    // '<mount>'(el){
+    //     console.log('dir attach', el);
+    // }
 }
+
+Dir.bootUp();
