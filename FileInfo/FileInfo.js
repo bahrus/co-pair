6ca9afe55cdd9fb6fadb8f1fe16767aa18362ea6
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-07-28 07:35:51 -0400

    Merge branch 'baseline' of https://github.com/bahrus/co-pair into baseline

---
+++ FileInfo/FileInfo.js
@@ -0,0 +1,40 @@
+//@ts-check
+/** @import {AP, Actions, PAP, } from './types' */
+/** @import {IshConfig } from '../ts-refs/trans-render/froop/types' */
+
+import {Scope} from 'trans-render/froop/Scope.js';
+
+/**
+ * @implements {Actions}
+ */
+export class FileInfo extends Scope{
+    /**
+     * @type {IshConfig<AP & Scope, Actions>}
+     */
+    static config = {
+        propInfo: {
+            handle: {},
+            name:{},
+        },
+        xform:{
+            '| name': 0,
+        },
+        compacts:{
+            when_handle_changes_call_getInfo: 0,
+        }
+    }
+
+    /**
+     * 
+     * @param {AP} self 
+     * @returns 
+     */
+    getInfo(self){
+        return /** @type {PAP} */({
+            name: self.handle.name,
+        });
+    }
+
+}
+
+FileInfo.bootUp();
---
+++ FileInfo/types.d.ts
@@ -0,0 +1,17 @@
+export interface EndUserProps {
+    handle: FileSystemHandle;
+}
+
+export interface AllProps extends EndUserProps {
+    name: string;
+}
+
+export type AP = AllProps;
+
+export type PAP = Partial<AllProps>;
+
+export type ProPAP = Promise<PAP>;
+
+export interface Actions {
+    getInfo(self: AP): PAP;
+}
