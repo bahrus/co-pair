69b6a9d1e60095408a438a840c80ff98388d0733
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-07-29 05:38:50 -0400

    Merge branch 'baseline' of https://github.com/bahrus/co-pair into baseline

--- DirList/types.d.ts
+++ DirList/types.d.ts
@@ -1,11 +1,9 @@
 import {PAP as DirInfoPAP, AP as DirInfoAllProps} from '../DirInfo/types';
 export interface EndUserProps {
-    handle: FileSystemDirectoryHandle;
 }
 
 export interface AllProps extends EndUserProps {
-    debugList: Array<DirInfoPAP>;
-    dirInfoRef: WeakRef<DirInfoAllProps>;
+    
 }
 
 export type AP = AllProps;
@@ -15,6 +13,5 @@ export type PAP = Partial<AllProps>;
 export type ProPAP = Promise<PAP>;
 
 export interface Actions {
-    hydrate(self: AP): PAP;
-    getList(self: AP): ProPAP;
+    
 }
