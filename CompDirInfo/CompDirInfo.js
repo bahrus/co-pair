de943c921f00dac2d61f23f913832c386adcdd01
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-08-03 08:27:30 -0400

    Merge branch 'baseline' of https://github.com/bahrus/co-pair into baseline

---
+++ CompDirInfo/CompDirInfo.js
@@ -0,0 +1,12 @@
+//@ts-check
+/** @import {AP, Actions, PAP, } from './types' */
+/** @import {IshConfig } from '../ts-refs/trans-render/froop/types' */
+
+import {Scope} from 'trans-render/froop/Scope.js';
+
+/**
+ * @implements {Actions}
+ */
+export class CompDirInfo extends Scope {
+    
+}
--- CompDirInfo/types.d.ts
+++ CompDirInfo/types.d.ts
@@ -8,6 +8,16 @@ export interface AllProps extends EndUserProps {
     subDirs: Array<SubDir>;
 }
 
+export type AP = AllProps;
+
+export type PAP = Partial<AllProps>;
+
+export type ProPAP = Promise<PAP>;
+
+export interface Actions {
+    getInfo(self: AP): ProPAP;
+}
+
 export interface SubDir {
     myHandle: FileSystemDirectoryHandle | undefined;
     yourHandle: FileSystemDirectoryHandle | undefined;
