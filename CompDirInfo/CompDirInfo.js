4666be3294f7df2c85baf3c970bae965b389f442
elimiller <eli.miller.anderson@gmail.com>
2025-08-07 17:37:20 -0400

    .

--- CompDirInfo/CompDirInfo.js
+++ CompDirInfo/CompDirInfo.js
@@ -50,19 +50,8 @@ export class CompDirInfo extends Scope {
         for await (const [name, yourSubHandle] of yourHandle.entries()){
             yourSubDirectories.push(yourSubHandle);
         }
-        //aplhabetize subdirectories
-        // mySubDirectories.sort((a, b) => a.name.localeCompare(b.name));
-        // yourSubDirectories.sort((a, b) => a.name.localeCompare(b.name));
-        //chatgpt suggested I use a map to speed up the lookup
-
-        // const yourHandleMap = new Map(
-        //     yourSubDirectories.map((subDir) => [subDir.name, subDir])
-
-        // );
         const yourHandlelMap = Object.groupBy(yourSubDirectories,subDir => subDir.name);
         const myHandleMap = Object.groupBy(mySubDirectories, subDir => subDir.name);
-        // It says the purpose of this line is to track names alreayd matched
-        //I however don't undestand how this is doing that
         const matches = new Set();
         for (const mySubHandle of mySubDirectories){
             const name = mySubHandle.name;
@@ -86,34 +75,6 @@ export class CompDirInfo extends Scope {
                 nameToDisplay: onlyYoursExists ? '' : name,
             });
         }
-        console.log(subDirs);
-        // for await (const mySubHandle of mySubDirectories){
-        //     const name = mySubHandle.name
-        //     const yourSubHandle = yourHandleMap.get(name);
-        //     subDirs.push({
-        //         myHandle : mySubHandle,
-        //         yourHandle: yourSubHandle,
-        //         weMatch: !!yourSubHandle,
-        //         onlyYoursExists: false,
-        //         onnlyMineExists: !yourSubHandle,
-        //         nameToDisplay: mySubHandle.name,
-        //     })
-        //     console.log(subDirs)
-
-
-
-        // }
-
-        //alphabetize mySubdictories and yourSubdirectories
-        //iterate through mySubdirectories
-        //if the name matches 
-        //push into subDirs
-        //iterate through yoursubs
-        // subDirs.push({
-        //     weMatch: true,
-        //     myHandle: 
-
-        // });
         return /** @type {PAP} */({
             name: myHandle.name,
             subDirs,
